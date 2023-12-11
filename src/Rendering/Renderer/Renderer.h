729dffea438c91c26d563a5ab6aab720439467fa
AnoukhW <anoukhman@gmail.com>
2023-12-11 21:24:13 +0100

    Fixed public and private specificator in Renderer

--- src/Rendering/Renderer/Renderer.h
+++ src/Rendering/Renderer/Renderer.h
@@ -3,10 +3,11 @@
 
 class Renderer
 {
+public:
     void SetRenderSize(unsigned int width, unsigned int height);
     virtual void Render() = 0;
     Texture& GetRenderedTexture();
-public:
+private:
     unsigned int renderHeight = 100;
     unsigned int renderWidth = 100;
     Texture renderedTexture;
