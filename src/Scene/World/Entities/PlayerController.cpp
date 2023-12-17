e83e7779f675816120bfce7825e075ca49ebbb56
AnoukhW <anoukhman@gmail.com>
2023-12-17 22:44:06 +0100

    Created PlayerController

---
+++ src/Scene/World/Entities/PlayerController.cpp
@@ -0,0 +1,51 @@
+﻿#include "PlayerController.h"
+
+#include "glad/glad.h"
+#include <GLFW/glfw3.h>
+
+#include "Entity.h"
+#include "Application/Application.h"
+
+void PlayerController::Init()
+{
+    inputManager = &Application::GetInstance().GetInputManager();
+}
+
+void PlayerController::Tick(float deltaTime)
+{
+    float playerSpeed = 0.4f;
+    float playerTurnSpeed = 6.0f;
+    if(PlayerController::player == nullptr)
+        return;
+
+    if (inputManager->IsKeyPressed(GLFW_KEY_W))
+    {
+        player->Foward(playerSpeed * deltaTime);
+    }
+    if (inputManager->IsKeyPressed(GLFW_KEY_S))
+    {
+        player->Foward(-playerSpeed * deltaTime);
+    }
+    if (inputManager->IsKeyPressed(GLFW_KEY_Q))
+    {
+        player->Strafe(playerSpeed * deltaTime);
+    }
+    if (inputManager->IsKeyPressed(GLFW_KEY_E))
+    {
+        player->Strafe(-playerSpeed * deltaTime);
+    }
+    if (inputManager->IsKeyPressed(GLFW_KEY_A))
+    {
+        player->Turn(playerTurnSpeed * deltaTime);
+    }
+    if (inputManager->IsKeyPressed(GLFW_KEY_D))
+    {
+        player->Turn(-playerTurnSpeed * deltaTime);
+    }
+}
+
+void PlayerController::SetPlayer(Entity* inPlayer)
+{
+    player = inPlayer;
+}
+
---
+++ src/Scene/World/Entities/PlayerController.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include "DLObject.h"
+
+class InputManager;
+class Entity;
+
+class PlayerController : public DLObject
+{
+public:
+    void Init() override;
+    void Tick(float deltaTime) override;
+
+    void SetPlayer(Entity* inPlayer);
+
+private:
+    Entity* player = nullptr;
+    InputManager* inputManager = nullptr;
+};
