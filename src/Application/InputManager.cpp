59bc71262125059e655f7a84bf6b9128fe53a5c9
AnoukhW <anoukhman@gmail.com>
2023-12-17 22:44:23 +0100

    Created InputManager

---
+++ src/Application/InputManager.cpp
@@ -0,0 +1,28 @@
+﻿#include "InputManager.h"
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+void InputManager::KeyboardKeyCallBackEvent(int key, int scancode, int action, int mods)
+{
+    if(action == GLFW_PRESS)
+    {
+        pressedKeys.push_front(key);
+    }
+    else if(action == GLFW_RELEASE)
+    {
+        pressedKeys.remove(key);
+    }
+}
+
+bool InputManager::IsKeyPressed(int key) const
+{
+    for(auto& pressedKey : pressedKeys)
+    {
+        if(pressedKey == key)
+        {
+            return true;
+        }
+    }
+    return false;
+}
---
+++ src/Application/InputManager.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include <list>
+#include <map>
+
+class InputManager
+{
+public:
+    void KeyboardKeyCallBackEvent(int key, int scancode, int action, int mods);
+
+    bool IsKeyPressed(int key) const;
+private:
+    std::list<int> pressedKeys;
+};
